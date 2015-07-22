060d73bdb24a8765e56f666d2fb450b260fc91f1
George Malayil <george.malayil@roguemonkey.in>
2015-07-22 17:14:11 +0530

    Switching to importing CocoaLumberjack.h (from DDLog.h) to suppress warnings about importing legacy macros.

--- Classes/RMPaperTrailLogger.h
+++ Classes/RMPaperTrailLogger.h
@@ -3,18 +3,25 @@
 //  PaperTrailLumberJack
 //
 //  Created by Malayil Philip George on 5/1/14.
-//  Copyright (c) 2014 Rogue Monkey Technologies & Systems Private Limited. All rights reserved.
+//  Copyright (c) 2014 Rogue Monkey Technologies & Systems Private Limited. All
+//  rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-#import <CocoaLumberjack/DDLog.h>
+#import <CocoaLumberjack/CocoaLumberjack.h>
 
 /**
- RMPaperTrailLogger is a custom logger (for CocoaLumberjack) that directs log output to your account on papertrailapp.com. It can log using TCP and UDP. On OS X, TLS is supported on TCP Connections. Currently, on iOS TCP connections only Plain-Text is supported. The default is UDP (which is always unencrypted). The logs are sent in a syslog format using RMSyslogFormatter. If you are going to provide a custom formatter, make sure that it formats messages that meets the syslog spec.
+ RMPaperTrailLogger is a custom logger (for CocoaLumberjack) that directs log
+ output to your account on papertrailapp.com. It can log using TCP and UDP. On
+ OS X, TLS is supported on TCP Connections. Currently, on iOS TCP connections
+ only Plain-Text is supported. The default is UDP (which is always unencrypted).
+ The logs are sent in a syslog format using RMSyslogFormatter. If you are going
+ to provide a custom formatter, make sure that it formats messages that meets
+ the syslog spec.
  */
 @interface RMPaperTrailLogger : DDAbstractLogger {
-    NSString *_host;
+    NSString* _host;
     NSUInteger _port;
     BOOL _useTcp;
     BOOL _useTLS;
@@ -23,7 +30,7 @@
 /**
  The host to which logs should be sent. Ex. logs.papertrailapp.com
  */
-@property (nonatomic, copy) NSString *host;
+@property (nonatomic, copy) NSString* host;
 
 /**
  The port on host to which we should connect. Ex. 9999
@@ -36,18 +43,21 @@
 @property (nonatomic, assign) BOOL useTcp;
 
 /**
- Specifies whether we should use TLS. Default is `YES`. This parameter applies only to TCP connections.
+ Specifies whether we should use TLS. Default is `YES`. This parameter applies
+ only to TCP connections.
  */
 @property (nonatomic, assign) BOOL useTLS;
 
 /**
  Returns a initialized singleton instance of this logger
  */
-+(RMPaperTrailLogger *) sharedInstance;
++ (RMPaperTrailLogger*)sharedInstance;
 
 /**
- Releases network resources (sockets) that were opened. Should be called when we are done with the logger. Any pending writes will be discarded when this method is called.
+ Releases network resources (sockets) that were opened. Should be called when we
+ are done with the logger. Any pending writes will be discarded when this method
+ is called.
  */
--(void) disconnect;
+- (void)disconnect;
 
 @end
